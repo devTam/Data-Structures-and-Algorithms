f2ec98549ecff09e514537274b3d255ad0667d2b
devTam <tammy.batubo@gmail.com>
2021-11-15 17:29:32 +0100

    implement tree

--- script.js
+++ script.js
@@ -469,3 +469,76 @@ myStackQueue.dequeue();
 myStackQueue.dequeue();
 // myStackQueue.dequeue();
 // console.log(myStackQueue);
+
+// ----------------BINARY SEARCH TREE-----------------
+
+class BSTNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new BSTNode(value);
+        if (this.root === null) {
+            this.root = newNode;
+        }else {
+            let currentNode = this.root;
+            while(true) {
+                if(value < currentNode.value) {
+                    if(!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left
+                }
+                if(value > currentNode.value) {
+                    if(!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+
+    lookup(value) {
+        if(!this.root) return false;
+        let currentNode = this.root;
+        while(currentNode) {
+            if(value < currentNode.value) {
+                currentNode = currentNode.left;
+            }else if(value > currentNode.value) {
+                currentNode = currentNode.right;
+            }else {
+                return currentNode;
+            }
+        }
+        return null;
+    }
+}
+
+const tree = new BinarySearchTree();
+// tree.insert(9);
+// tree.insert(4);
+// tree.insert(6);
+// tree.insert(20);
+// tree.insert(170);
+// tree.insert(15);
+// tree.insert(1);
+// console.log(JSON.stringify(traverse(tree.root)));
+// console.log(tree.lookup(2));
+
+function traverse(node) {
+    const tree =  { value: node.value }
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
+}
