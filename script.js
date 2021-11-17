057b2c8e26e427414a552893523a4d121bd6ab68
devTam <tammy.batubo@gmail.com>
2021-11-17 05:29:04 +0100

    add graphs

--- script.js
+++ script.js
@@ -542,3 +542,47 @@ function traverse(node) {
     tree.right = node.right === null ? null : traverse(node.right);
     return tree;
 }
+
+class Graph {
+    constructor() {
+        this.numberOfNodes = 0;
+        this.adjacentList = {};
+    }
+
+    addVertex(node) {
+        this.adjacentList[node] = [];
+        this.numberOfNodes++;
+    }
+
+    addEdge(node1, node2) {
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
+    }
+
+    showConnections() {
+        const allNodes = Object.keys(this.adjacentList);
+        for (let node of allNodes) {
+            let nodeConnections = this.adjacentList[node];
+            let connections = "";
+            let vertex;
+            for (vertex of nodeConnections) {
+                connections += vertex + " ";
+            }
+            console.log(node + "-->" + connections);
+        }
+    }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex("0");
+myGraph.addVertex("1");
+myGraph.addVertex("2");
+myGraph.addVertex("3");
+myGraph.addVertex("4");
+myGraph.addVertex("5");
+myGraph.addEdge("3", "1");
+myGraph.addEdge("3", "4");
+myGraph.addEdge("4", "2");
+myGraph.addEdge("4", "5");
+
+myGraph.showConnections();
