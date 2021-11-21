6f95c553f5e437b53c555ba99cf399579f588e2f
devTam <tammy.batubo@gmail.com>
2021-11-22 00:42:12 +0100

    implement selection sort algo

--- algo.js
+++ algo.js
@@ -50,4 +50,22 @@ const bubbleSort = (arr) => {
     return arr;
 }
 
-console.log(bubbleSort(numbers));
+// ----- Selection Sort ------->
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort(numbers));
