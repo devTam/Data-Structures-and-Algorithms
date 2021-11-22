074d5cae7665efba082d11150f800436882acd72
devTam <tammy.batubo@gmail.com>
2021-11-22 01:37:52 +0100

    implement merge sort algo

--- algo.js
+++ algo.js
@@ -83,4 +83,27 @@ const insertionSort = (arr) => {
     return arr;
 }
 
+// ----- merge Sort ------->
+const mergeSort = (arr) => {
+    if (arr.length < 2) {
+        return arr;
+    }
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = (left, right) => {
+    let result = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+    return result.concat(left, right);
+}
+
 console.log(insertionSort(numbers));
