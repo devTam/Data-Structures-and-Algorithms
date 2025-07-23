2defd3fdde5ef33ed0f460e0926b9b4170298a4c
devTam <tammy.batubo@gmail.com>
2025-07-23 18:01:55 +0100

    two sum

---
+++ blind75.js
@@ -0,0 +1,32 @@
+// 1. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)   
+// Using Maps
+const twoSum = (nums, target) => {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
+
+// Using Objects
+const twoSum2 = (nums, target) => {
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map[complement] !== undefined) {
+            return [map[complement], i];
+        }
+        map[nums[i]] = i;
+    }
+    return [];
+}
