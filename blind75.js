4566433bce725a35c41519dcbc86dd083b35dc66
devTam <tammy.batubo@gmail.com>
2025-07-23 19:34:45 +0100

    buy and sell stocks

--- blind75.js
+++ blind75.js
@@ -30,3 +30,29 @@ const twoSum2 = (nums, target) => {
     }
     return [];
 }
+
+// 2. Best Time to Buy and Sell Stock
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+const maxProfit = (prices) => {
+    let buy = 0;
+    let sell = 1;
+    let maxProfit = 0;
+    while(sell < prices.length) { 
+        if(prices[sell] < prices[buy]) {
+            buy = sell
+        }else {
+            const profit = prices[sell] - prices[buy];
+            if(profit > maxProfit) {
+                maxProfit = profit
+            }
+        }
+        sell++
+    }
+    return maxProfit;
+}   
+
